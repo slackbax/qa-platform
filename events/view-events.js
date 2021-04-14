39dc58ce29407c33492f677e3372e583556e5a6c
slackbax <i.munoz.j@gmail.com>
2021-04-14 16:17:24 -0400

    Bug busqueda historica

--- events/view-events.js
+++ events/view-events.js
@@ -49,6 +49,6 @@ $(document).ready(function () {
 	});
 
 	$('#view-historic').click(function () {
-		tableFiles.ajax.url('events/ajax.getServerAccidents.php').load();
+		tableFiles.ajax.url('events/ajax.getServerEvents.php').load();
 	});
 });
