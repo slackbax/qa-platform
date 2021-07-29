85a2426acb0c837449458a8dbfd7d1d0e8736bbf
slackbax <i.munoz.j@gmail.com>
2021-07-29 15:56:43 -0400

    envio de correo por evento de tecnovigilancia

--- tecnoevents/ajax.insertEvent.php
+++ tecnoevents/ajax.insertEvent.php
@@ -1,9 +1,11 @@
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
 
 session_start();
 include("../class/classMyDBC.php");
 include("../class/classTecnoEvento.php");
+include("../class/classServicio.php");
 include("../src/fn.php");
 include("../src/sessionControl.ajax.php");
 require("../vendor/autoload.php");
@@ -11,6 +13,7 @@ require("../vendor/autoload.php");
 if (extract($_POST)):
 	$db = new myDBC();
 	$ev = new TecnoEvento();
+	$se = new Servicio();
 	$idate = setDateBD($idate);
 	$idateev = (!empty($idateev)) ? setDateBD($idateev) : NULL;
 	$idatefab = (!empty($idatefab)) ? setDateBD($idatefab) : NULL;
@@ -29,7 +32,6 @@ if (extract($_POST)):
 		$db->Commit();
 		$db->autoCommit(TRUE);
 
-		/*
 		$serv = $se->get($iserv);
 		$eve = $ev->get($ins['msg']);
 		$msg = '<b>ATENCIÓN</b><br>';
@@ -43,7 +45,6 @@ if (extract($_POST)):
 		$msg .= '<br><br><br>Saludos cordiales,<br><br>Plataforma de Calidad y Seguridad del Paciente';
 
 		$mail = new PHPMailer;
-
 		$mail->IsSMTP();
 		$mail->SMTPDebug = 0;
 		$mail->SMTPAuth = true;
@@ -52,34 +53,23 @@ if (extract($_POST)):
 		$mail->Port = 587;
 		$mail->Username = "ti.hggb@gmail.com";
 		$mail->Password = "svr1504_woot";
-
 		$mail->SetFrom('soportedesarrollo@ssconcepcion.cl', 'Plataforma Calidad');
-
 		$mail->Subject = "Mensaje desde plataforma calidad";
 		$mail->AltBody = "Para visualizar el mensaje, por favor utilice un visor de correos compatible con HTML!"; // optional, comment out and test
-
-		if ((int)$istev < 13 or (int)$istev == 15):
-			$mail->MsgHTML(utf8_decode($msg));
-		else:
-			$mail->MsgHTML(utf8_decode("Se ha generado un nuevo ingreso de reporte de evento adverso/centinela con el código <b>" . $ins['msg'] . "</b> y paciente <b>" . $iname . "</b>. <br><br>Saludos cordiales,<br><br>Plataforma de Calidad y Seguridad del Paciente"));
-		endif;
+		$mail->MsgHTML(utf8_decode($msg));
 
 		// Testing only
 		// $address = "i.munoz.j@gmail.com";
 		// $mail->AddAddress("i.munoz.j@gmail.com", "Yo");
 		// $mail->AddAddress("soportedesarrollo@ssconcepcion.cl", "Soporte");
 
-		$mail->AddAddress("cmunoz@ssconcepcion.cl", "Claudia Munoz");
-		$mail->AddAddress("lgatica@ssconcepcion.cl", "Luis Gatica");
-
-		if ((int)$istev < 13 or (int)$istev == 15):
-			$mail->AddAddress("boportus@ssconcepcion.cl", "Boris Oportus");
-			$mail->AddAddress("kcampos@ssconcepcion.cl", "Katherine Campos");
-		endif;
+		$mail->AddAddress("cmunoz@ssconcepcion.cl", "Enf. Claudia Munoz");
+		$mail->AddAddress("leilavielma@ssconcepcion.cl", "Enf. Leila Vielma");
+		$mail->AddAddress("miguelaguayo@ssconcepcion.cl", "Dr. Miguel Aguayo");
 
 		if (!$mail->send()):
 			throw new Exception('Error al enviar correo de confirmación. ' . $mail->ErrorInfo, 0);
-		endif;*/
+		endif;
 
 		$response = array('type' => true, 'msg' => 'OK');
 		echo json_encode($response);
